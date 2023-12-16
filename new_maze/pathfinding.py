29611f920b7d3c565c07a26e1987a482dfcc904b
Kamal Mohamed <kamal.mohamed@msa.edu.eg>
2023-12-16 11:52:30 +0200

    Abstraction

--- new_maze/pathfinding.py
+++ new_maze/pathfinding.py
@@ -3,6 +3,8 @@ import pygame
 import sys
 from algorithms import Algorithms
 
+
+
 window_width = 700
 window_height = 700
 
@@ -14,10 +16,6 @@ rows = 25
 box_width = window_width // columns
 box_height = window_height // rows
 
-grid = []
-queue = []
-
-algorithm = Algorithms()
 
 class Box:
     def __init__(self, i, j):
@@ -33,9 +31,13 @@ class Box:
         self.heuristic = None
 
     def draw(self, win, color):
-        pygame.draw.rect(win, color, (self.x * box_width, self.y * box_height, box_width-2, box_height-2))
+        pygame.draw.rect(
+            win,
+            color,
+            (self.x * box_width, self.y * box_height, box_width - 2, box_height - 2),
+        )
 
-    def set_neighbours(self):
+    def set_neighbours(self,grid):
         if self.x > 0:
             self.neighbours.append(grid[self.x - 1][self.y])
         if self.x < columns - 1:
@@ -46,25 +48,52 @@ class Box:
             self.neighbours.append(grid[self.x][self.y + 1])
 
 
-# Create Grid
-for i in range(columns):
-    arr = []
-    for j in range(rows):
-        arr.append(Box(i, j))
-    grid.append(arr)
+def generate_grid():
+    grid = []
+    for i in range(columns):
+        arr = []
+        for j in range(rows):
+            arr.append(Box(i, j))
+        grid.append(arr)
+    return grid
 
 # Set Neighbours
-for i in range(columns):
-    for j in range(rows):
-        grid[i][j].set_neighbours()
+def set_neighbours(grid):
+    for i in range(columns):
+        for j in range(rows):
+            grid[i][j].set_neighbours(grid)
 
-start_box = grid[0][0]
-start_box.start = True
-start_box.visited = True
-queue.append(start_box)
+def draw_boxes(box,paths):
+    box.draw(window, (100, 100, 100))
+    if box.queued:
+        box.draw(window, (200, 0, 0))
+    if box.visited:
+        box.draw(window, (0, 200, 0))
+    if box in paths:
+        box.draw(window, (0, 0, 200))
+
+    if box.start:
+        box.draw(window, (0, 200, 200))
+    if box.wall:
+        box.draw(window, (10, 10, 10))
+    if box.target:
+        box.draw(window, (200, 200, 0))
+
+
+def init_start(grid):
+    queue = []
+    box = grid
+    box.start = True
+    box.visited = True
+    queue.append(box)
+    return box,queue
 
 
 def main():
+    algorithm = Algorithms()
+    grid = generate_grid()
+    set_neighbours(grid)
+    start_box,queue = init_start(grid[0][0])
     begin_search = False
     target_box_set = False
     target_box = None
@@ -97,34 +126,21 @@ def main():
             # Start Algorithm
             if event.type == pygame.KEYDOWN and target_box_set:
                 begin_search = True
-            
+
         if begin_search:
-            #algorithm.Astar_search(grid,rows, columns, [x_coor,y_coor])
-            searching = algorithm.depth_first_search(queue, start_box,target_box,searching,paths)
+            # algorithm.Astar_search(grid,rows, columns, [x_coor,y_coor])
+            searching = algorithm.depth_first_search(
+                queue, start_box, target_box, searching, paths
+            )
 
         window.fill((0, 0, 0))
 
         for i in range(columns):
             for j in range(rows):
                 box = grid[i][j]
-                box.draw(window, (100, 100, 100))
-
-                if box.queued:
-                    box.draw(window, (200, 0, 0))
-                if box.visited:
-                    box.draw(window, (0, 200, 0))
-                if box in paths:
-                    box.draw(window, (0, 0, 200))
-
-                if box.start:
-                    box.draw(window, (0, 200, 200))
-                if box.wall:
-                    box.draw(window, (10, 10, 10))
-                if box.target:
-                    box.draw(window, (200, 200, 0))
-
+                draw_boxes(box,paths)
+            
         pygame.display.flip()
 
 
 main()
-
